470102f6118288280325a6fe8cf28467a49d4f83
Cédric <cchabaud@interludesante.com>
2023-01-04 17:41:44 +0100

    update firebase login via env file for github

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,27 @@
 import { Manga } from "./manga";
+require('dotenv').config();
 
 const firebase = require("firebase-admin");
-const serviceAccount = require(".././mangareminder-d3ade-firebase-adminsdk-3fnz5-e3642b393c.json");
 
 export default class Firebase {
-    fes
+
     protected database: any
 
     constructor() {
+
+        const serviceAccount = {
+            "type": process.env.type,
+            "project_id": process.env.project_id,
+            "private_key_id": process.env.private_key_id,
+            "private_key": process.env.private_key,
+            "client_email": process.env.client_email,
+            "client_id": process.env.client_id,
+            "auth_uri": process.env.auth_uri,
+            "token_uri": process.env.token_uri,
+            "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+            "client_x509_cert_url": process.env.client_x509_cert_url
+        }
+
         const app = firebase.initializeApp({
             credential: firebase.credential.cert(serviceAccount),
             databaseURL: "https://mangareminder-d3ade-default-rtdb.europe-west1.firebasedatabase.app"
