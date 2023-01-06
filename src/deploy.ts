b042f5b0c35fffa2b732b7e394564cb5da8e0feb
Cédric <cchabaud@interludesante.com>
2023-01-07 00:12:52 +0100

    remove https

--- src/deploy.ts
+++ src/deploy.ts
@@ -7,22 +7,22 @@ require('dotenv').config();
 export default class Deploy {
 
     readonly webSites: Array<String> = [
-        'https://mangarockteam.com/manga/after-being-reborn-i-became-the-strongest-to-save-everyone/',
-        'https://mangarockteam.com/manga/global-martial-arts/',
-        'https://mangarockteam.com/manga/onepunch-man/',
-        'https://mangarockteam.com/manga/mushoku-tensei-isekai-ittara-honki-dasu/',
-        'https://mangarockteam.com/manga/tate-no-yuusha-no-nariagari/',
-        'https://mangarockteam.com/manga/hajime-no-ippo/',
-        'https://mangarockteam.com/manga/tensei-shitara-slime-datta-ken_2/',
-        'https://mangarockteam.com/manga/solo-max-level-newbie/',
-        'https://mangarockteam.com/manga/the-world-after-the-fall/',
-        'https://mangarockteam.com/manga/the-tutorial-is-too-hard/',
-        'https://mangarockteam.com/manga/the-player-that-cant-level-up/',
-        'https://mangarockteam.com/manga/the-breaker-eternal-force/',
-        'https://mangarockteam.com/manga/return-of-the-sss-class-ranker/',
-        'https://mangarockteam.com/manga/return-of-the-frozen-player/',
-        'https://mangarockteam.com/manga/my-status-as-an-assassin-obviously-exceeds-the-braves/',
-        'https://mangarockteam.com/manga/blue-lock/',
+        'http://mangarockteam.com/manga/after-being-reborn-i-became-the-strongest-to-save-everyone/',
+        'http://mangarockteam.com/manga/global-martial-arts/',
+        'http://mangarockteam.com/manga/onepunch-man/',
+        'http://mangarockteam.com/manga/mushoku-tensei-isekai-ittara-honki-dasu/',
+        'http://mangarockteam.com/manga/tate-no-yuusha-no-nariagari/',
+        'http://mangarockteam.com/manga/hajime-no-ippo/',
+        'http://mangarockteam.com/manga/tensei-shitara-slime-datta-ken_2/',
+        'http://mangarockteam.com/manga/solo-max-level-newbie/',
+        'http://mangarockteam.com/manga/the-world-after-the-fall/',
+        'http://mangarockteam.com/manga/the-tutorial-is-too-hard/',
+        'http://mangarockteam.com/manga/the-player-that-cant-level-up/',
+        'http://mangarockteam.com/manga/the-breaker-eternal-force/',
+        'http://mangarockteam.com/manga/return-of-the-sss-class-ranker/',
+        'http://mangarockteam.com/manga/return-of-the-frozen-player/',
+        'http://mangarockteam.com/manga/my-status-as-an-assassin-obviously-exceeds-the-braves/',
+        'http://mangarockteam.com/manga/blue-lock/',
     ]
     public firebase: any
     public webhook: any
