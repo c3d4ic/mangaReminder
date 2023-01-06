8e77b4e4991a29a3e9e3629a13cd37d8f0f0b121
Cédric <cchabaud@interludesante.com>
2023-01-06 23:22:41 +0100

    every minute

--- netlify/functions/schedule.ts
+++ netlify/functions/schedule.ts
@@ -1,10 +1,10 @@
 import { schedule } from "@netlify/functions"
 import Deploy from "./../../src/deploy"
 
-export const handler = schedule("*/15 * * * *", async (event: any) => {
+export const handler = schedule("* * * * *", async (event: any) => {
 
     console.log("Scheduled function executed")
-    // const deploy = new Deploy()
+    const deploy = new Deploy()
 
     return {
         statusCode: 200,
