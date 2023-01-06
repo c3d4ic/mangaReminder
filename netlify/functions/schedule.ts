66024ab14151946ec0a16b98df437438a5186f9e
Cédric <cchabaud@interludesante.com>
2023-01-06 19:13:51 +0100

    add deploy

--- netlify/functions/schedule.ts
+++ netlify/functions/schedule.ts
@@ -1,41 +1,12 @@
 import { schedule } from "@netlify/functions"
+import Deploy from "./../../src/deploy"
 
-
-// export const handler = async (event: any) => {
-
-//     return {
-//         statusCode: 200,
-//         body: "Hello from schedule function"
-//     }
-// }
-
-
-export const handler = schedule("* * * * *", async (event: any) => {
+export const handler = schedule("*/15 * * * *", async (event: any) => {
 
     console.log("Scheduled function executed")
+    const deploy = new Deploy()
 
     return {
         statusCode: 200,
-        // body: "Hello from schedule function"
     }
 })
-
-
-
-
-// import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
-// import Deploy from "./../../src/deploy"
-
-// const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-//     console.log("Received event:", event);
-
-//     const deploy = new Deploy()
-
-//     return {
-//         statusCode: 200,
-//     };
-// };
-
-// const handler = schedule("*/15 * * * *", myHandler)
-
-// export { handler };
