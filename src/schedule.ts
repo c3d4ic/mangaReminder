f5ed0687038e743b3e7d30fea89b68b4960fe7db
Cédric <cchabaud@interludesante.com>
2023-01-06 09:26:27 +0100

    build up

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,11 +1,25 @@
-import { schedule } from "@netlify/functions"
+// import { schedule } from "@netlify/functions"
 
-export const handler = schedule("* * * * *", async (event: any) => {
+// export const handler = schedule("* * * * *", async (event: any) => {
 
-    console.log("Scheduled function executed")
+//     console.log("Scheduled function executed")
+
+//     return {
+//         statusCode: 200,
+//         body: "Hello from schedule function"
+//     }
+// })
+
+import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+
+const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    console.log("Received event:", event);
 
     return {
         statusCode: 200,
-        body: "Hello from schedule function"
-    }
-})
+    };
+};
+
+const handler = schedule("@hourly", myHandler)
+
+export { handler };
