e45bfaacf0bf9d46b4f2f8458d1715a6397be048
Cédric <cchabaud@interludesante.com>
2023-01-06 19:11:18 +0100

    up schedule

--- netlify/functions/schedule.ts
+++ netlify/functions/schedule.ts
@@ -1,28 +1,41 @@
-// import { schedule } from "@netlify/functions"
+import { schedule } from "@netlify/functions"
 
-// export const handler = schedule("* * * * *", async (event: any) => {
 
-//     console.log("Scheduled function executed")
+// export const handler = async (event: any) => {
 
 //     return {
 //         statusCode: 200,
 //         body: "Hello from schedule function"
 //     }
-// })
+// }
 
-import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
-import Deploy from "./../../src/deploy"
 
-const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    console.log("Received event:", event);
+export const handler = schedule("* * * * *", async (event: any) => {
 
-    const deploy = new Deploy()
+    console.log("Scheduled function executed")
 
     return {
         statusCode: 200,
-    };
-};
+        // body: "Hello from schedule function"
+    }
+})
 
-const handler = schedule("*/15 * * * *", myHandler)
 
-export { handler };
+
+
+// import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+// import Deploy from "./../../src/deploy"
+
+// const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+//     console.log("Received event:", event);
+
+//     const deploy = new Deploy()
+
+//     return {
+//         statusCode: 200,
+//     };
+// };
+
+// const handler = schedule("*/15 * * * *", myHandler)
+
+// export { handler };
