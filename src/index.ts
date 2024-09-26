ed520a9fdbadcf549f7ff02ecd3643950ba2e5c9
Cédric <c3d4ic@gmail.com>
2024-09-26 19:56:05 +0200

    Ajout du docker file

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@
 import Server from "./server";
 import Deploy from "./deploy";
 
-const server = new Server(6001)
+const server = new Server(3000)
 const deploy = new Deploy()
 
 var cron = require('node-cron');
+
 cron.schedule('0 8-23/2 * * *', () => {
+    const date = new Date();
+    console.log("Update le ",date)
     deploy.run()
 });
 
